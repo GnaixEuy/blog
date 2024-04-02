aa5db320618947b5aff2472698f121eae0c5132e
GnaixEuy <SGGHCJ@live.com>
2024-04-03 03:17:47 +0800

    Site updated: 2024-04-03 03:17:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/hello-world/","2024/04/02/MarkDown的基本语法使用/"];function toRandomPost(){
+var posts=["2024/04/02/MarkDown的基本语法使用/","2024/03/31/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
