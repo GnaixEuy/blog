e85540bbf26ca639c7981881688bfd1da27c0128
GnaixEuy <SGGHCJ@live.com>
2024-04-19 14:05:17 +0800

    Site updated: 2024-04-19 14:05:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/07/Jetbrain系列快捷键-Win版/","2024/04/19/Stream流操作，让代码更加的优雅/","2024/03/31/hello-world/","2024/04/02/MarkDown的基本语法使用/"];function toRandomPost(){
+var posts=["2024/04/07/Jetbrain系列快捷键-Win版/","2024/04/02/MarkDown的基本语法使用/","2024/04/19/Stream流操作，让代码更加的优雅/","2024/03/31/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
